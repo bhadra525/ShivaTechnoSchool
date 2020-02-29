7233b8c8a29956a04ad6aeb3a814238c027af6f7
bhadra525 <bhadra.govindarajula@gmail.com>
2020-02-29 17:41:35 +0530

    gy

--- src/main/java/com/rts/VO/StudentInfo.java
+++ src/main/java/com/rts/VO/StudentInfo.java
@@ -2,7 +2,13 @@ package com.rts.VO;
 
 import java.util.Date;
 
-public class StudentInfo {
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public @Data class StudentInfo {
 	
 	private String studentName;
 	private String standard;
@@ -13,68 +19,6 @@ public class StudentInfo {
 	private Date dob;
 	private Date jod;
 	private String motherName;
-	
-	
 	private boolean isactive;
-	public String getStudentName() {
-		return studentName;
-	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	public String getStandard() {
-		return standard;
-	}
-	public void setStandard(String standard) {
-		this.standard = standard;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-	public String getFatherName() {
-		return fatherName;
-	}
-	public void setFatherName(String fatherName) {
-		this.fatherName = fatherName;
-	}
-	public String getCameFrom() {
-		return cameFrom;
-	}
-	public void setCameFrom(String cameFrom) {
-		this.cameFrom = cameFrom;
-	}
-	public Date getDob() {
-		return dob;
-	}
-	public void setDob(Date dob) {
-		this.dob = dob;
-	}
-	public Date getJod() {
-		return jod;
-	}
-	public void setJod(Date jod) {
-		this.jod = jod;
-	}
-	public String getMotherName() {
-		return motherName;
-	}
-	public void setMotherName(String motherName) {
-		this.motherName = motherName;
-	}
-	public boolean isIsactive() {
-		return isactive;
-	}
-	public void setIsactive(boolean isactive) {
-		this.isactive = isactive;
-	}
-	
+
 }
---
+++ src/main/java/com/rts/controller/StudentController.java
@@ -0,0 +1,5 @@
+package com.rts.controller;
+
+public class StudentController {
+
+}
